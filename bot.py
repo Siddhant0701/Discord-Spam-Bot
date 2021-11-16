434f392923fb4003b79e5b0fef6afa2c95328aaa
Siddhant0701 <mahajansiddhant07@gmail.com>
2021-11-15 22:57:28 -0500

    feat: add functionality for sentences

--- bot.py
+++ bot.py
@@ -20,13 +20,18 @@ async def on_message(message):
 	
     if message.content.startswith('$spam '):
         user_msg = message.content.split()
-        spam_user = user_msg[1]
+        sentence_array = user_msg[1:len(user_msg)-1]
 
-        spam_count = int(user_msg[2])
+        spam_count = int(user_msg[len(user_msg)-1])
         num = min(MAXIMUM_SPAM, spam_count)
-        
+
+        sentence = ""
+        for word in sentence_array:
+            sentence += word + " "
+        sentence = sentence[:len(sentence)-1]
+
         for i in range(num):
-            await message.channel.send(spam_user)
+            await message.channel.send(sentence)
 
     elif (message.content.startswith('$spam')):
         await message.channel.send('Incorrect Usage')
